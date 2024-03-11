b9c6a1beb59e3c924d54a38ebb926e58dfd2f00d
Yaqwb Naqeb <Yaqub-naqeb>
2024-03-11 18:08:21 +0300

    feat: adding searchBy

--- src/pages/books/_index.jsx
+++ src/pages/books/_index.jsx
@@ -1,10 +1,6 @@
 import BookCard from "../../components/buttons/cards/BookCard";
 import { gettingBooks } from "../../api/DataFetcher/BookFetcher";
-import {
-  Link,
-  useLoaderData,
-  useSearchParams,
-} from "react-router-dom";
+import { Link, useLoaderData, useSearchParams } from "react-router-dom";
 import { useCallback, useState } from "react";
 import Pagination from "../../components/buttons/Pagination";
 
@@ -12,11 +8,13 @@ export const booksLoader = async ({ request }) => {
   const url = new URL(request.url);
   const searchValue = url.searchParams.get("searchValue");
   let currentPage = url.searchParams.get("page");
+  const searchBy = url.searchParams.get("searchBy");
+
 
   let booksData = {};
   try {
     booksData = await gettingBooks(
-      `http://127.0.0.1:8000/api/books/?title=${
+      `http://127.0.0.1:8000/api/books/?${searchBy ? searchBy : "?"}=${
         searchValue
           ? searchValue
           : `&page_size=2&page=${currentPage ? currentPage : 1}`
@@ -39,6 +37,8 @@ export default function Books() {
   const booksData = useLoaderData();
   const totalBooks = booksData.count;
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchBy, setSearchBy] = useState("title");
+  const [booked,SetBooked]=useState('None');
 
   const totalPages = Math.ceil(totalBooks / 2); // 10 books per page
   const handlePageClick = (number) => {
@@ -47,9 +47,11 @@ export default function Books() {
 
   const handleSearchClick = useCallback(() => {
     const updatedSearchParams = new URLSearchParams(searchParams);
+    updatedSearchParams.set("searchBy", searchBy);
+
     updatedSearchParams.set("searchValue", searchValue);
     setSearchParams(updatedSearchParams);
-  }, [searchParams, setSearchParams, searchValue]);
+  }, [searchParams, setSearchParams, searchValue, searchBy]);
 
   const handleSearchValue = useCallback(
     (e) => {
@@ -57,31 +59,95 @@ export default function Books() {
 
       if (!e.target.value) {
         const updatedSearchParams = new URLSearchParams(searchParams);
-        updatedSearchParams.delete("searchvalue");
+        updatedSearchParams.delete("searchBy");
+        updatedSearchParams.delete("searchValue");
         setSearchParams(updatedSearchParams);
       }
     },
     [setSearchParams, searchParams]
   );
 
+  const options = [
+    { value: "title", label: "Title" },
+    { value: "author", label: "Author" },
+    { value: "dewey_decimal", label: "Dewey decimal" },
+  ];
+
+  const isBooked=[
+    { value: "none", label: "None" },
+    { value: "booked", label: "Booked" },
+    { value: "Notbooked", label: "Not booked" },
+    
+  ]
+
+  const handleSearchOptions = useCallback(
+    (option) => {
+      setSearchBy(option.target.value);
+    },
+    [setSearchBy]
+  );
+  const handleIsBookedOptions = useCallback(
+    (option) => {
+      SetBooked(option.target.value);
+    },
+    [SetBooked]
+  );
+
   return (
     <div>
+
+
+<div>
+
+
       <div className="search">
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchValue}
-          onChange={handleSearchValue}
-          className="search__input"
-        />
-        <button
-          type="button"
-          className="search__button"
-          onClick={handleSearchClick}
-        >
-          search
-        </button>
+        <div className="searchBy">
+          <label htmlFor="selectInput">Search By </label>
+          <select
+            id="selectInput"
+            value={searchBy}
+            defaultValue={searchBy}
+            onChange={handleSearchOptions}
+          >
+            {/* <option value="">--Please choose an option--</option> */}
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* search */}
+        <div>
+          {" "}
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchValue}
+            onChange={handleSearchValue}
+            className="search__input"
+          />
+          <button
+            type="button"
+            className="search__button"
+            onClick={handleSearchClick}
+          >
+            search
+          </button>
+        </div>
       </div>
+
+
+
+
+
+
+</div>
+
+
+
+
       <div className="book-card-list">
         {booksData?.results?.map((book) => (
           <Link key={book.id} to={`http://localhost:5173/books/${book.id}`}>
